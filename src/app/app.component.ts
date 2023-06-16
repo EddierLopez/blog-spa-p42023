ec41483bec8c1284732b456e598b0d1dcd4a647b
Eddier Lopez <eddier.lopez.lopez@una.cr>
2023-06-16 12:41:58 -0600

    Actualización al 16-6-2023 mañana

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,23 @@
-import { Component } from '@angular/core';
-
-
+import { Component} from '@angular/core';
+import { UserService } from './services/user.service';
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:[UserService]
 })
 export class AppComponent {
-  constructor(   
-    
+  public identity:any;
+  private checkIdentity;
+  constructor(
+    private _userService:UserService
   ){
-
-  }
+    this.checkIdentity=setInterval(()=>{
+      this.identity=this._userService.getIdentity();
+    },1000);
+  }   
+  
   
 }
